d1780de5fbf71750809d9687c0d19594c7e5cc35
eric <kuchaguangjie@gmail.com>
2022-01-08 17:55:41 +0800

    ...

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -11,7 +11,8 @@ use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), ParseIntError> {
+type AnyErrorResult<T> = std::result::Result<T, Box<dyn error::Error>>;
+fn main() -> AnyErrorResult<()> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
